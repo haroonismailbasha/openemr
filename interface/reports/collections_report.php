6a1c9f7caa92378c9c666191e5a36027153eaa64
haroonismailbasha <haroon.ismailbasha@gmail.com>
2025-08-31 15:46:04 -0400

    Splitted the CPT Query. Temporary Commit. This will be moved to separate report

--- interface/reports/collections_report.php
+++ interface/reports/collections_report.php
@@ -611,43 +611,7 @@ else if (!empty($_POST['download_csv'])) {
     }
     $timestamp = date('Y-m-d_H-i-s');
     $sqlArray = array();
-    $query = "SELECT f.id, f.date, f.pid, CONCAT(w.lname, ', ', w.fname) AS provider_id, f.encounter, f.last_level_billed,IF(b.billed = 0, 'Unbilled', 'Billed') AS billing_status,  " .
-        "f.last_level_closed, f.last_stmt_date, f.stmt_count, f.invoice_refno, f.in_collection, " .
-        "p.fname, p.mname, p.lname, p.street, p.city, p.state, " .
-        "p.postal_code, p.phone_home, p.ss, p.billing_note, " .
-        "p.pubpid, p.DOB, CONCAT(u.lname, ', ', u.fname) AS referrer, " .
-        "( SELECT bill_date FROM billing AS b WHERE " .
-        "b.pid = f.pid AND b.encounter = f.encounter AND " .
-        "b.activity = 1 AND b.code_type != 'COPAY' LIMIT 1) AS bill_date, " .
-        "( SELECT SUM(b.fee) FROM billing AS b WHERE " .
-        "b.pid = f.pid AND b.encounter = f.encounter AND " .
-        "b.activity = 1 AND b.code_type != 'COPAY' ) AS charges, " .
-        "( SELECT SUM(b.fee) FROM billing AS b WHERE " .
-        "b.pid = f.pid AND b.encounter = f.encounter AND " .
-        "b.activity = 1 AND b.code_type = 'COPAY' ) AS copays, " .
-        "( SELECT SUM(s.fee) FROM drug_sales AS s WHERE " .
-        "s.pid = f.pid AND s.encounter = f.encounter ) AS sales, " .
-        "( SELECT SUM(a.pay_amount) FROM ar_activity AS a WHERE " .
-        "a.pid = f.pid AND a.encounter = f.encounter AND a.deleted IS NULL) AS payments, " .
-        "( SELECT SUM(a.adj_amount) FROM ar_activity AS a WHERE " .
-        "a.pid = f.pid AND a.encounter = f.encounter AND a.deleted IS NULL) AS adjustments, " .
-        "cpt.cpt_codes " .
-        "FROM form_encounter AS f " .
-        "JOIN patient_data AS p ON p.pid = f.pid " .
-        "/** haroon start **/ JOIN 	billing AS b ON f.pid=b.pid /** haroon end **/" .
-        "LEFT OUTER JOIN users AS u ON u.id = f.referring_provider_id " .
-        "LEFT OUTER JOIN users AS w ON w.id = f.provider_id " .
-        "LEFT JOIN (
-            SELECT
-                pid,
-                encounter,
-                GROUP_CONCAT(DISTINCT code ORDER BY code SEPARATOR ',') AS cpt_codes
-            FROM billing
-            WHERE code_type = 'CPT4' AND activity = 1
-            GROUP BY pid, encounter
-            ) cpt ON cpt.pid = f.pid AND cpt.encounter = f.encounter " .
-        "WHERE $where " .
-        "ORDER BY f.pid, f.encounter";
+    $query = "SELECT f.id, f.date, f.pid, CONCAT(w.lname, ', ', w.fname) AS provider_id, f.encounter, f.last_level_billed, f.last_level_closed, f.last_stmt_date, f.stmt_count, f.invoice_refno, f.in_collection, p.fname, p.mname, p.lname, p.street, p.city, p.state, p.postal_code, p.phone_home, p.ss, p.billing_note, p.pubpid, p.DOB, CONCAT(u.lname, ', ', u.fname) AS referrer, (SELECT bill_date FROM billing AS b WHERE b.pid = f.pid AND b.encounter = f.encounter AND b.activity = 1 AND b.code_type != 'COPAY' LIMIT 1) AS bill_date, (SELECT SUM(b.fee) FROM billing AS b WHERE b.pid = f.pid AND b.encounter = f.encounter AND b.activity = 1 AND b.code_type != 'COPAY') AS charges, (SELECT SUM(b.fee) FROM billing AS b WHERE b.pid = f.pid AND b.encounter = f.encounter AND b.activity = 1 AND b.code_type = 'COPAY') AS copays, (SELECT SUM(s.fee) FROM drug_sales AS s WHERE s.pid = f.pid AND s.encounter = f.encounter) AS sales, (SELECT SUM(a.pay_amount) FROM ar_activity AS a WHERE a.pid = f.pid AND a.encounter = f.encounter AND a.deleted IS NULL) AS payments, (SELECT SUM(a.adj_amount) FROM ar_activity AS a WHERE a.pid = f.pid AND a.encounter = f.encounter AND a.deleted IS NULL) AS adjustments, cpt.code AS cpt_codes FROM form_encounter AS f JOIN patient_data AS p ON p.pid = f.pid JOIN billing AS b ON f.pid = b.pid LEFT OUTER JOIN users AS u ON u.id = f.referring_provider_id LEFT OUTER JOIN users AS w ON w.id = f.provider_id LEFT JOIN (SELECT pid, encounter, code FROM billing WHERE code_type = 'CPT4' AND activity = 1) cpt ON cpt.pid = f.pid AND cpt.encounter = f.encounter WHERE b.code_type like '%' AND cpt.code is not null ORDER BY f.pid, f.encounter, cpt.code;";
 
     $eres = sqlStatement($query, $sqlArray);
 
