16dea6933a134fb5c9a3e722e5910525bb3071a8
haroonismailbasha <haroon.ismailbasha@gmail.com>
2025-09-07 19:41:49 -0400

    commit after alab ar report modification as per WA query

--- interface/reports/alab_ar_report.php
+++ interface/reports/alab_ar_report.php
@@ -234,7 +234,7 @@ if (!empty($_POST['download_csv'])) {
     $rows = array();
     $where = "";
     // HAROON_CHANGE_4_START_08262025
-    $where .= " /** haroon start **/ b.code_type like '%' /** haroon end **/ ";
+    // $where .= " /** haroon start **/ b.code_type like '%' /** haroon end **/ ";
     //HAROON_CHANGE_4_END_08262025
 
     // $sqlArray = array();
@@ -250,13 +250,16 @@ if (!empty($_POST['download_csv'])) {
             $where .= "f.date >= ? AND f.date <= ? ";
             array_push($sqlArray, $form_date . ' 00:00:00', $form_date . ' 23:59:59');
         }
+
+        $where .= " ORDER BY f.pid, f.encounter; ";
+
     }
 
     if (! $where) {
         $where = "1 = 1";
     }
     $timestamp = date('Y-m-d_H-i-s');
-    $query = "SELECT f.id, f.date, f.pid, CONCAT(w.lname, ', ', w.fname) AS provider_id, f.encounter, f.last_level_billed,IF(b.billed = 0, 'Unbilled', 'Billed') AS billing_status,  f.last_level_closed, f.last_stmt_date, f.stmt_count, f.invoice_refno, f.in_collection, p.fname, p.mname, p.lname, p.street, p.city, p.state, p.postal_code, p.phone_home, p.ss, p.billing_note, p.pubpid, p.DOB, CONCAT(u.lname, ', ', u.fname) AS referrer, (SELECT bill_date FROM billing AS b WHERE b.pid = f.pid AND b.encounter = f.encounter AND b.activity = 1 AND b.code_type != 'COPAY' LIMIT 1) AS bill_date, (SELECT SUM(b.fee) FROM billing AS b WHERE b.pid = f.pid AND b.encounter = f.encounter AND b.activity = 1 AND b.code_type != 'COPAY') AS charges, (SELECT SUM(b.fee) FROM billing AS b WHERE b.pid = f.pid AND b.encounter = f.encounter AND b.activity = 1 AND b.code_type = 'COPAY') AS copays, (SELECT SUM(s.fee) FROM drug_sales AS s WHERE s.pid = f.pid AND s.encounter = f.encounter) AS sales, a.pay_amount AS payments, a.adj_amount AS adjustments, cpt.code AS cpt_codes FROM form_encounter AS f JOIN patient_data AS p ON p.pid = f.pid JOIN billing AS b ON f.pid = b.pid LEFT OUTER JOIN users AS u ON u.id = f.referring_provider_id LEFT OUTER JOIN users AS w ON w.id = f.provider_id LEFT JOIN (SELECT pid, encounter, code FROM billing WHERE code_type = 'CPT4' AND activity = 1) cpt ON cpt.pid = f.pid AND cpt.encounter = f.encounter LEFT JOIN ar_activity AS a ON a.pid = f.pid AND a.encounter = f.encounter AND a.deleted IS NULL AND " .
+    $query = "SELECT f.id, f.date, f.pid, CONCAT(w.lname, ', ', w.fname) AS provider_id, f.encounter, f.last_level_billed, f.last_level_closed, f.last_stmt_date, f.stmt_count, f.invoice_refno, f.in_collection, p.fname, p.mname, p.lname, p.street, p.city, p.state, p.postal_code, p.phone_home, p.ss, p.billing_note, p.pubpid, p.DOB, CONCAT(u.lname, ', ', u.fname) AS referrer, ( SELECT bill_date FROM billing AS b WHERE b.pid = f.pid AND b.encounter = f.encounter AND b.activity = 1 AND b.code_type != 'COPAY' LIMIT 1) AS bill_date, ( SELECT SUM(b.fee) FROM billing AS b WHERE b.pid = f.pid AND b.encounter = f.encounter AND b.activity = 1 AND b.code_type != 'COPAY' ) AS charges, ( SELECT SUM(b.fee) FROM billing AS b WHERE b.pid = f.pid AND b.encounter = f.encounter AND b.activity = 1 AND b.code_type = 'COPAY' ) AS copays, ( SELECT SUM(s.fee) FROM drug_sales AS s WHERE s.pid = f.pid AND s.encounter = f.encounter ) AS sales, ( SELECT SUM(a.pay_amount) FROM ar_activity AS a WHERE a.pid = f.pid AND a.encounter = f.encounter AND a.deleted IS NULL) AS payments, ( SELECT SUM(a.adj_amount) FROM ar_activity AS a WHERE a.pid = f.pid AND a.encounter = f.encounter AND a.deleted IS NULL) AS adjustments, cpt.cpt_codes, CASE WHEN b.billed = 1 THEN 'Billed' ELSE 'Not billed' END AS billing_status FROM form_encounter AS f JOIN patient_data AS p ON p.pid = f.pid JOIN billing AS b ON f.pid=b.pid LEFT OUTER JOIN users AS u ON u.id = f.referring_provider_id LEFT OUTER JOIN users AS w ON w.id = f.provider_id LEFT JOIN ( SELECT pid, encounter, GROUP_CONCAT(DISTINCT code ORDER BY code SEPARATOR ',') AS cpt_codes FROM billing WHERE code_type = 'CPT4' AND activity = 1 GROUP BY pid, encounter ) cpt ON cpt.pid = f.pid AND cpt.encounter = f.encounter where " .
         $where ;
         // " ORDER BY f.pid, f.encounter, cpt.code;";
 
